9717f7d1d15656ce7839153f6d6256cd09b7ef18
h3llrais3r <pooh_beer_1@hotmail.com>
2018-12-12 21:49:05 +0100

    Make parser py3 compatible
    urllib3 always returns byte responses, so decode if needed because json needs string type.

--- tvdb_api_v2/client/parser.py
+++ tvdb_api_v2/client/parser.py
@@ -2,7 +2,7 @@
 
 import json
 
-from six import iteritems
+from six import iteritems, string_types
 
 from tvdb_api_v2.models.series_episodes_summary import SeriesEpisodesSummary
 from tvdb_api_v2.models.series_search_result import SeriesSearchResult
@@ -18,7 +18,7 @@ def parse_series_episodes_summary(response):
     >>> params = {'imdb_id': imdb_id, '_preload_content': False}
     >>> parser.parse_series_episodes_summary(SeriesApi(self.api_client).series_id_episodes_summary_get(**params))
     """
-    data = json.loads(response.data)
+    data = _response_to_json(response)
     # The json object is a dict with a data key which contains a SeriesEpisodesSummary object
     # The result is an object of type SeriesEpisodesSummary
     result = data['data'] if 'data' in data.keys() else None
@@ -32,7 +32,7 @@ def parse_search_series_results(response, best_result=False):
     >>> params = {'imdb_id': imdb_id, '_preload_content': False}
     >>> parser.parse_search_series(SearchApi(self.api_client).search_series_get(**params))
     """
-    data = json.loads(response.data)
+    data = _response_to_json(response)
     # The json object is a dict with a data key which contains a list of SeriesSearchResult
     # The result is an object of type SeriesSearchResults
     search_results = data['data'] if 'data' in data.keys() else None
@@ -40,6 +40,12 @@ def parse_search_series_results(response, best_result=False):
         return SeriesSearchResults([_deserialize_model(search_results[0], SeriesSearchResult())])
     else:
         return SeriesSearchResults([_deserialize_model(result, SeriesSearchResult()) for result in search_results])
+
+
+def _response_to_json(response):
+    # urllib3 always returns byte responses, so decode if needed because json needs string type
+    return json.loads(response.data) if isinstance(response.data, string_types) else json.loads(
+        response.data.decode('utf-8'))
 
 
 def _deserialize_model(data, instance):
